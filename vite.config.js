909e8c634c27fd97f14c07b07b8d9a04ace26eb5
FaisalDipto <dipto22d@gmail.com>
2026-03-20 16:14:01 +0600

    Fix config and basename

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/app',        // ← uncomment this
+  server: {
+    host: '127.0.0.1',
+    port: 3000,
+    allowedHosts: ['meharaz733.com', 'www.meharaz733.com'],
+  }
 })
