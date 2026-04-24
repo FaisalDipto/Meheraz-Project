02c43bf32e03e6b3b8b6a23b3ec6dd9856e9e9b2
FaisalDipto <dipto22d@gmail.com>
2026-04-24 21:09:05 +0600

    Optimize the cat animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
+import React, { useEffect } from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import Footer from '../components/Footer'
 import FAQ from '../components/FAQ'
 import '../App.css'
+import catAnimationUrl from '../../animation/catLottieJSON.json?url'
 
 export default function Home() {
+  useEffect(() => {
+    // Preload the Get Started cat animation so it's instantly available from cache
+    fetch(catAnimationUrl).catch(() => {});
+  }, []);
+
   return (
     <div className="bg-background font-body text-on-surface antialiased selection:bg-primary selection:text-on-primary">
       <Navbar />
